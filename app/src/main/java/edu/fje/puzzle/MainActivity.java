37a8e24cd26099c7796af506bb8d081de0c730ff
Jordi Velasco <030177.clot@fje.edu>
2020-02-12 20:50:45 +0100

    first commit

---
+++ app/src/main/java/edu/fje/puzzle/MainActivity.java
@@ -0,0 +1,84 @@
+package edu.fje.puzzle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    Handler hnd;
+    Runnable rn;
+    Button pl;
+    boolean p = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pl = (Button) findViewById(R.id.bPlpa);
+        pl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (p == true) {
+                    p = false;
+                    pl.setText("Play");
+                    onPause();
+                } else {
+                    p = true;
+                    pl.setText("Pause");
+                    onResume();
+                }
+            }
+        });
+
+        hnd = new Handler();
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.song);
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                playCycle();
+                mp.start();
+            }
+        });
+
+    }
+
+    public void playCycle(){
+        if(mp.isPlaying()){
+            rn = new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
+                }
+            };
+            hnd.postDelayed(rn, 1000);
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+        mp.pause();
+    }
+
+    public void onResume(){
+        super.onResume();
+        mp.start();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mp.release();
+        hnd.removeCallbacks(rn);
+    }
+}

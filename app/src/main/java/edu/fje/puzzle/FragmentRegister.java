a2e0f402e2347d1acf0415b18ff8e7e1eed10c06
Alellandrou <34569583+Alellandrou@users.noreply.github.com>
2020-02-19 19:30:24 +0100

    fragments

---
+++ app/src/main/java/edu/fje/puzzle/FragmentRegister.java
@@ -0,0 +1,114 @@
+package edu.fje.puzzle;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FragmentRegister extends Fragment {
+
+    Button bLogin, bRegister;
+    EditText eUser, eMail, ePass;
+    CallbackFragment callbackFragment;
+    String user, mail, password;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    FirebaseAuth fAuth;
+    DatabaseReference db;
+
+    @Override
+    public void onAttach(Context context) {
+        sharedPreferences = context.getSharedPreferences("usersFile", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.register_fragment,container,false);
+        fAuth = FirebaseAuth.getInstance();
+        eUser = view.findViewById(R.id.eUsuario);
+        eMail = view.findViewById(R.id.eMail);
+        ePass = view.findViewById(R.id.eContra);
+
+
+        bRegister = view.findViewById(R.id.bRegister);
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = eUser.getText().toString().trim();
+                mail = eMail.getText().toString().trim();
+                password = ePass.getText().toString().trim();
+
+
+                editor.putString("userName", user);
+                editor.putString("email", mail);
+                editor.putString("pass", password);
+                editor.apply();
+
+                if(user!=null && mail!=null && password!=null){
+                    registerUser();
+                }
+            }
+        });
+        return view;
+    }
+
+    private void registerUser(){
+        fAuth.createUserWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                         String id = fAuth.getCurrentUser().getUid();
+                        Map<String, Object> map = new HashMap<>();
+
+                        map.put("name", user);
+                        map.put("email", mail);
+                        map.put("password", password);
+
+                         db.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                             @Override
+                             public void onComplete(@NonNull Task<Void> task2) {
+                                 if(task2.isSuccessful()){
+                                     Toast.makeText(getContext(),"Registrado", Toast.LENGTH_SHORT).show();
+                                   //  startActivity(new Intent(FragmentRegister.this,MainActivity.class));
+                                 }
+                             }
+                         });
+
+
+                        }else{
+                         Toast.makeText(getContext(),"Error de registro", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+}
--- app/src/main/java/edu/fje/puzzle/MainActivity.java
+++ app/src/main/java/edu/fje/puzzle/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.fje.puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -8,14 +11,22 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements CallbackFragment {
 
     MediaPlayer mp;
     Handler hnd;
     Runnable rn;
-    Button pl;
+    Button pl, p2,p3;
     boolean p = true;
+    Fragment fragment;
+    FragmentManager fm;
+    FragmentTransaction ft;
+    FirebaseAuth fAuth;
 
 
     @Override
@@ -24,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pl = (Button) findViewById(R.id.bPlpa);
+        p2 = (Button) findViewById(R.id.button);
+        p3 = (Button) findViewById(R.id.button2);
+
+        fAuth = FirebaseAuth.getInstance();
+
+
+        p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment();
+            }
+        });
+
+        p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFragment();
+            }
+        });
+
         pl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
         hnd = new Handler();
         mp = MediaPlayer.create(getApplicationContext(), R.raw.song);
         mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
         mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
@@ -81,4 +111,48 @@ public class MainActivity extends AppCompatActivity {
         mp.release();
         hnd.removeCallbacks(rn);
     }
+
+    public void addFragment(){
+        FragmentLogin fragment = new FragmentLogin();
+        fragment.setCallbackFragment(this);
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.add(R.id.fragmentContainer,fragment);
+        ft.commit();
+    }
+
+    public void replaceFragment(){
+        fragment= new FragmentRegister();
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.addToBackStack(null);
+        ft.replace(R.id.fragmentContainer,fragment);
+        ft.commit();
+    }
+
+    public void startFragment(){
+        FragmentMain newFragment = new FragmentMain();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack if needed
+        transaction.replace(R.id.fragmentContainer, newFragment);
+        transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
+    }
+
+    @Override
+    public void changeFragment() {
+        replaceFragment();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
 }
